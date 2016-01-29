ab471819d8a6d420ab657206e2fd464508f6363b
Eric Gardner <gardner.ec@gmail.com>
2016-01-28 17:12:20 -0800

    Extract logic from TOC into helper methods

---
+++ helpers/catalogue_helpers.rb
@@ -0,0 +1,15 @@
+module CatalogueHelpers
+  # Sort Catalogue Contents
+  # returns an array of resource objects ordered by sort_order attribute
+  def sort_catalogue_contents
+    contents        = sitemap.resources.find_all {|p| p.data.sort_order }
+    sorted_contents = contents.sort_by {|p| p.data.sort_order }
+  end
+
+  # Lookup Catalogue Entry
+  # expects a cat number (int)
+  # returns a hash of entry data or nil if no entry is found
+  def lookup_entry(cat)
+    data.catalogue.find { |entry| entry.cat == cat }
+  end
+end
