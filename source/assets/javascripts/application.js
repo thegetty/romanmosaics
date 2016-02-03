52c9280d8f02c83159f9ecda0409e82df71f0a3b
Eric Gardner <gardner.ec@gmail.com>
2016-02-03 13:02:34 -0800

    Add "curtain" effect to slideout nav
    When the slideout nav is active, a new curtain element has been added to
    dim the page background for a better user experience.

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -4,9 +4,15 @@ $(document).ready(function() {
   // Sidebar nav toggle
   var $sidebar = $(".nav-contents");
   var $menuButton = $("#navbar-menu");
+  var $curtain = $(".sliding-panel-fade-screen");
 
   $menuButton.on("click touchstart", function() {
     $sidebar.toggleClass("is-visible");
+    $curtain.toggleClass("is-visible");
   });
 
+  $curtain.on("click touchstart", function() {
+    $sidebar.removeClass("is-visible");
+    $curtain.removeClass("is-visible");
+  });
 });
