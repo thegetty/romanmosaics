1bfb42034af3dffa4234f2998397bc5fda626123
Eric Gardner <gardner.ec@gmail.com>
2016-02-02 16:45:36 -0800

    Enable basic navbar slideout behavior
    Right now this can be switched from a left to right layout by changing
    the value of the $sliding-panel-position in _nav_contents.scss.
    
    A mobile version (full-screen slide-out) should probably be created
    next. Also, there should be a button to close the menu within the menu
    itself in this case.

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,1 +1,12 @@
 //= require_tree .
+
+$(document).ready(function() {
+  // Sidebar nav toggle
+  var $sidebar = $(".nav-contents");
+  var $menuButton = $("#navbar-menu");
+
+  $menuButton.on("click touchstart", function() {
+    $sidebar.toggleClass("is-visible");
+  });
+
+});
