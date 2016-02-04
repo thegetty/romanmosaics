79ecca1d4214618aa7b678cc56edfee932760b77
Eric Gardner <gardner.ec@gmail.com>
2016-02-03 16:21:32 -0800

    Add page transitions
    Despite all the trends towards single-page apps with routing frameworks,
    etc., SmoothState remains an excellent tool for improving both
    performance and user experience while embracing the spirit of
    progressive enhancement. The project continues to be actively developed
    and it is easy to leverage external libraries like velocity.js for
    fine-grained animations.
    
    This commit represents the most basic setup for the plugin. In the main
    application template, content has been organized under a #main element
    inside of the <body> tag. Everything of importance lives here
    (navigation elements included).
    
    In the main application.js file, SmoothState is initialized on this
    container element. The options passed to it define the behavior at
    different stages. At minimum for nice transitions you should define an
    onStart and onReady object. Finally, use the onAfter hook to
    re-initialize any UI elements (in this case, keyboard nav and off-canvas
    sliding panel).
    
    It's amazing how much the addition of a simple transition between pages
    improves the subjective experience of using the app.

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -5,4 +5,24 @@ $(document).ready(function() {
   keyboardNav();
   offCanvasNav();
 
+  // smoothState init
+  $("#main").smoothState({
+    onStart: {
+      duration: 400,
+      render: function($container) {
+        $container.velocity('fadeOut', { duration: 200 });
+      }
+    },
+    onReady: {
+      duration: 400,
+      render: function($container, $newContent) {
+        $container.html($newContent);
+        $container.velocity('fadeIn', { duration: 100 });
+      }
+    },
+    onAfter: function($container, $newContent) {
+      keyboardNav();
+      offCanvasNav();
+    }
+  });
 });
