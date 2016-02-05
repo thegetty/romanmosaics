2672a29a9831a3a304ae0840d6827773ee75ba3f
Eric Gardner <gardner.ec@gmail.com>
2016-02-05 15:12:03 -0800

    Client-side search with Vue and Lunr.js
    This commit is a big step forward for the project. We now have working
    client-side full-text search using lunr.js, as in the Terracottas
    project. However, the organization of the code has been greatly improved
    and the Handlebars.js dependency has been removed in favor of Vue.
    
    This should be much easier to reason about going forward, as all
    search-related logic is organized into clearly defined methods.
    
    - Dependencies
    This commit adds the vue-resource library for simplified HTTP requests
    inside of vue components. This is in addition to the previously
    committed dependencies on Vue.js and Lunr.js. Because this project does
    not use NPM, all of these js dependencies have been committed to source.
    I'm really beginning to see this as an upside, as NPM modules change so
    quickly it makes it hard to be confident in the reproducibility of a
    build.
    
    - Template updates
    Vue.js requires some custom attributes in templates (v-bind, @click,
    etc.) similar to how Angular works. Haml seems to choke on these by
    default, so all templates which are going to be used by a Vue instance
    should be written in ERB. Fortunately there is no problem with mixing
    partials and layouts that have been written in different languages.
    
    One important note: the Vue instance which handles search and results
    needs access to the entire .navbar component; users will enter queries
    in the navbar search input, and then results will be shown in an
    adjacent search-results element.
    
    To manage this, the entire contents of the nav partial have been nested
    under a #js-nav div. Also, the entire partial has been re-written in
    ERB for compatibility.
    
    - Styles
    New style rules targeting .search-results, .search-results-inner, and
    .search-results-list-item have been added to the .search.scss file.
    
    - Search Logic
    All search logic now lives in the search.js file, organized as a Vue
    instance.
    
    One thing lives outside of this instance in the global namespace for
    now: a SearchIndex variable which is created by Lunr. This needs to
    remain outside Vue because it has its own methods (like search()) which
    the Search vue instance needs to be able to call.
    
    Actual result matching is done by using Vue's list-rendering and a
    computed property of searchResults. This is an array of data that
    combines Lunr's index matches with an array of the actual site contents
    (to get title/url to the resulting pages).
    
    - Interaction with SmoothState
    This was my main concern in using Vue for elements which will appear
    across pages (instead of just on a single page), but things seem to be
    working fine. The key is to manage the creation/destruction of
    instances in Smoothstate's hooks. So vue instances should be created
    when the page first loads (inside of $(document).ready()); they should
    be destroyed (using vm.$destroy()) inside the onStart callback, and then
    re-created in the onAfter callback.
    
    To clear out the user's last search after changing pages, I have also
    added a destroy hook to the vue instance which resets the query to an
    empty string. This seems to provide the expected behavior without any
    problems so far.

---
+++ source/assets/javascripts/search.js
@@ -0,0 +1,79 @@
+//= require vendor/vue
+//= require vendor/vue-resource.min
+//= require vendor/lunr.min
+
+
+// Make search index global for now
+var SearchIndex = lunr(function () {
+  this.field('title', { boost: 100 });
+  this.field('contents', { boost: 10 });
+  this.field('location', { boost: 50 });
+  this.field('material', { boost: 10 });
+  this.field('culture', { boost: 10 });
+  this.field('inv', { boost: 10 });
+});
+
+// Vue component
+Vue.config.debug = true;
+
+var Search = {
+  el: "#js-nav",
+  data: {
+    contents: [],
+    query: "",
+    loaded: false,
+    url: "/search.json"
+  },
+  computed: {
+    searchResults: function () {
+      var results = [];
+      var query = this.$get("query");
+      var contents = this.$get("contents");
+
+      SearchIndex.search(query).forEach(function(result) {
+        var item = {
+          title: contents[result.ref].title,
+          url: contents[result.ref].url
+        };
+
+        results.push(item);
+      })
+
+      return this.loaded ? results : [];
+    }
+  },
+  methods: {
+    // Stash search.json data into contents array to cross-reference against
+    // search results
+    contentList: function (data) {
+      this.$set("contents", data)
+    },
+    // Load the search.json data via AJAX and feed it to the Lunr index instance
+    // This function should only be called through explicit action on the part
+    // of the user, as it makes a network request for a potentially large
+    // amount of data (a few hundred KB for full text search)
+    loadSearchIndex: function () {
+      if ( !this.loaded ) {
+        this.$http({url: this.url, method: 'GET'}).then(function(response) {
+          this.lunrSetup(response.data);
+          this.contentList(response.data);
+          this.$set("loaded", true);
+        }, function (response) {
+          console.log("Error...");
+        });
+      }
+    },
+    // Feed json data of site text content into the pre-existing Lunr index
+    // so it can be queried
+    lunrSetup: function (data) {
+      // Populate the index with our data
+      data.forEach(function (item) {
+        SearchIndex.add(item);
+      });
+    },
+  },
+  destroyed: function () {
+    this.$set("query", "");
+    console.log("Destroyed successfully");
+  }
+}
