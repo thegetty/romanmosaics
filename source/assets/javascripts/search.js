2a4e6da3b114035c2e6b84aaac3731d6468695d8
Eric Gardner <gardner.ec@gmail.com>
2016-02-05 16:19:44 -0800

    Revert "Set search URL to work on GH Pages for now"
    This reverts commit dad19eb5ce2611b2fcaa34f9ef992deede4883a4.

--- source/assets/javascripts/search.js
+++ source/assets/javascripts/search.js
@@ -22,7 +22,7 @@ var Search = {
     contents: [],
     query: "",
     loaded: false,
-    url: "http://gettypubs.github.io/mosaics/search.json"
+    url: "/search.json"
   },
   computed: {
     searchResults: function () {
@@ -33,7 +33,7 @@ var Search = {
       SearchIndex.search(query).forEach(function(result) {
         var item = {
           title: contents[result.ref].title,
-          url: "http://gettypubs.github.io/mosaics" + contents[result.ref].url
+          url: contents[result.ref].url
         };
 
         results.push(item);
