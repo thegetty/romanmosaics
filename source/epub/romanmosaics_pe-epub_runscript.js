1854f522beea262c8ab2d8bb795c9fec5edeb5eb
Greg Albers <geealbers@gmail.com>
2016-03-29 15:57:43 -0700

    remove runscript

--- source/epub/romanmosaics_pe-epub_runscript.js
+++
@@ -1,8 +0,0 @@
-var Peepub   = require('pe-epub-fs')(require('pe-epub'));
-var epubJson = require('/Users/galbers/GitHub/mosaics/source/epub/romanmosaics.json'); // see examples/example.json for the specs
-var myPeepub = new Peepub(epubJson);
-
-myPeepub.create('/Users/galbers/GitHub/mosaics/source/assets/downloads/RomanMosaics_Belis.epub')
-    .then(function(filePath){
-        console.log(filePath); // the same path to your epub file!
-    });
